4a2baa8ea2ed420944a2c7e01cd8dbab5106897b
Bear Revels <157622412+bear-revels@users.noreply.github.com>
2024-02-29 15:11:12 +0100

    Update clean.py

--- utils/clean.py
+++ utils/clean.py
@@ -1,1 +1,9 @@
-placeholder
+import pandas as pd
+
+# Importing the data
+clean_data = pd.read_csv("https://raw.githubusercontent.com/bear-revels/immo-eliza-scraping-Python_Pricers/main/data/all_property_details.csv")
+
+# create data cleaning function
+
+# Storing locally
+clean_data.to_csv('./src/raw/clean_data.csv')
