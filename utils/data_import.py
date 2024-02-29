7a5f83eefec538842f64ee78d251fae6b18b0664
Viktor Cosaert <viktor.cosaert@gmail.com>
2024-02-29 14:46:19 +0100

    created repo skeleton

--- utils/data_import.py
+++ utils/data_import.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
+# Importing the data
 raw_data = pd.read_csv("https://raw.githubusercontent.com/bear-revels/immo-eliza-scraping-Python_Pricers/main/data/all_property_details.csv")
-print(raw_data.info())
+
+# Storing locally
+raw_data.to_csv('./src/raw/raw_data.csv')
