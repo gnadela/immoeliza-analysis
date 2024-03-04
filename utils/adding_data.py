ff4e017858804eed89dd420ae88fd2d2684eb5a1
Viktor Cosaert <viktor.cosaert@gmail.com>
2024-03-04 12:19:27 +0100

    still figuring out population density

--- utils/adding_data.py
+++ utils/adding_data.py
@@ -6,10 +6,9 @@ postal_refnis = pd.read_csv('../src/external_data/Postal_Refnis.csv')
 sector_data = pd.read_csv('../src/external_data/SectorData.csv')
 
 # A Prepping postal_refnis for join
-postal_refnis['CD_REFNIS'] = postal_refnis['Refnis code']
-postal_refnis = postal_refnis[['Postal code', 'CD_REFNIS', 'Gemeentenaam']]
-pr = postal_refnis.rename(columns={'Postal code': 'PostalCodes'})
-pr_aggregated = pr.groupby('CD_REFNIS')['PostalCodes'].apply(lambda x: ','.join(x.astype(str))).reset_index()
+postal_refnis = postal_refnis[['Postal code', 'Refnis code', 'Gemeentenaam']]
+pr = postal_refnis.rename(columns={'Postal code': 'PostalCode', 'Refnis code': 'CD_REFNIS'})
+pr_aggregated = pr.groupby('CD_REFNIS')['PostalCode'].apply(lambda x: ','.join(x.astype(str))).reset_index()
 # B Prepping sector_data for join
 sector_data = sector_data[['CD_REFNIS', 'TOTAL', 'OPPERVLAKKTE IN HM²']]
     # Refniscodes are split in even smaller sections -> merge them to overcome issues (take into account in interpretations)
@@ -25,8 +24,10 @@ merged_data['population density'] = (merged_data['TOTAL'] / merged_data['Area in
 # Display the first few rows of the DataFrame to verify the new column
 
 print(merged_data.head())
+large_area_combinations = merged_data[merged_data['PostalCode'].str.len() > 15]
 
 merged_data.to_csv('../src/external_data/PopulationDensity.csv')
+large_area_combinations.to_csv('../src/external_data/checkforlargeareaaggregations.csv')
 
 # join population density to main cleaned based on postal code -> join on if in postalcodes
     # Import the csv into a new df
