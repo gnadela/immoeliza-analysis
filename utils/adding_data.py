ec70381256838e5f0dca662009d45f9c1414963d
Bear Revels <bear.revels@gmail.com>
2024-03-05 13:04:51 +0100

    Merge branch 'main' of https://github.com/gnadela/immoeliza-analysis

--- utils/adding_data.py
+++ utils/adding_data.py
@@ -1,44 +1,40 @@
 import pandas as pd
 
-# 0
-# setup dataframes
-# origin: https://statbel.fgov.be/
-postal_refnis = pd.read_csv('../src/external_data/Postal_Refnis.csv')
-sector_data = pd.read_csv('../src/external_data/SectorData.csv')
+def add_population_density(cleaned_data_file):
+    # Importing external dataframes
+    conversion_key = pd.read_csv('./src/external_data/Postal_Refnis.csv')
+    sector_data = pd.read_csv('./src/external_data/SectorData.csv')
 
-# 1
-# join postal_refnis[postal code] to sector data, based on refnis code -> [CD_REFNIS]
-
-# A Prepping postal_refnis for join
-postal_refnis['CD_REFNIS'] = postal_refnis['Refnis code']
-postal_refnis = postal_refnis[['Postal code', 'CD_REFNIS', 'Gemeentenaam']]
-pr = postal_refnis.rename(columns={'Postal code': 'PostalCodes'})
-# B Prepping sector_data for join
-sector_data = sector_data[['CD_REFNIS', 'TOTAL', 'OPPERVLAKKTE IN HM²']]
+    # Prepping postal_refnis for join
+    conversion_key = conversion_key[['Postal code', 'Refnis code', 'Gemeentenaam']]
+    conversion_key = conversion_key.rename(columns={'Postal code': 'PostalCode', 'Refnis code': 'CD_REFNIS'})
+    # Accounting for overlap in refniscodes
+    conversion_aggregated = conversion_key.groupby('CD_REFNIS')['PostalCode'].apply(lambda x: ','.join(x.astype(str))).reset_index()
+    # Prepping sector_data for join
+    sector_data = sector_data[['CD_REFNIS', 'TOTAL', 'OPPERVLAKKTE IN HM²']]
     # Refniscodes are split in even smaller sections -> merge them to overcome issues (take into account in interpretations)
-aggregated_sector_data = sector_data.groupby('CD_REFNIS', as_index=False).agg({'TOTAL': 'sum', 'OPPERVLAKKTE IN HM²': 'sum'})
+    aggregated_sector_data = sector_data.groupby('CD_REFNIS', as_index=False).agg({'TOTAL': 'sum', 'OPPERVLAKKTE IN HM²': 'sum'})
+    # Merging
+    merged_data = pd.merge(aggregated_sector_data, conversion_aggregated, on='CD_REFNIS', how='left')
 
-pr_aggregated = pr.groupby('CD_REFNIS')['PostalCodes'].apply(lambda x: ','.join(x.astype(str))).reset_index()
+    # Calculating population density (population per km²)
+    merged_data['Area in km²'] = merged_data['OPPERVLAKKTE IN HM²'] * 0.01
+    merged_data['PopulationDensity'] = (merged_data['TOTAL'] / merged_data['Area in km²']).round().astype(int)
 
-merged_data = pd.merge(aggregated_sector_data, pr_aggregated, on='CD_REFNIS', how='left')
+    # Importing cleaned data into a df
+    original = pd.read_csv(cleaned_data_file)
+    # Drop all but postalcode & population density
+    to_merge = merged_data[['PostalCode', 'PopulationDensity']]
 
+    # Iterating over postalcode string 
+    postal_code_density = {}
+    for _, row in to_merge.iterrows():
+        postal_codes = row['PostalCode'].split(',')
+        for code in postal_codes:
+            postal_code_density[code] = row['PopulationDensity']
 
-# 2
-# calc population density column
+    # Adding a new column for population density in original, based on the mapping
+    original['PopulationDensity'] = original['PostalCode'].apply(lambda x: postal_code_density.get(str(x), None))
 
-# Calculate population density (population per km²)
-merged_data['Area in km²'] = merged_data['OPPERVLAKKTE IN HM²'] * 0.01
-merged_data['population density'] = (merged_data['TOTAL'] / merged_data['Area in km²']).round(2)
-
-# Display the first few rows of the DataFrame to verify the new column
-
-print(merged_data.head())
-
-merged_data.to_csv('../src/external_data/PopulationDensity.csv')
-
-# join population density to main cleaned based on postal code -> join on if in postalcodes
-    # Import the csv into a new df
-
-    # Drop all but postalcode & density
-
-    # Add to clean data
+    # Resaving new data with added column over cleaned_data
+    original.to_csv(cleaned_data_file, index=False)
